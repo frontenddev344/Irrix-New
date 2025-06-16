e0e3eb686abd24d52fb70ac46546a888f1e75f67
dev-avology <devavology12@gmail.com>
2025-06-16 18:24:32 +0530

    update

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open");
+
+  // Toggle icon between ☰ and ✕
+  if (mobileMenu.classList.contains("open")) {
+    hamburger.innerHTML = "&times;"; // ✕
+  } else {
+    hamburger.innerHTML = "&#9776;"; // ☰
+  }
+});
